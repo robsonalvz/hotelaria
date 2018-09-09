cb78646441869499e39f6aaddd06e7ce96dbcfdf
robsonalvz <robson.alves@dce.ufpb.br>
2018-09-09 10:32:53 -0300

    metodo pesquisar adicionado

--- app/src/main/java/pratica/curso/com/hotelaria/HotelListFragment.java
+++ app/src/main/java/pratica/curso/com/hotelaria/HotelListFragment.java
@@ -1,9 +1,10 @@
 package pratica.curso.com.hotelaria;
 
 import android.app.Activity;
-import android.app.ListFragment;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -20,8 +21,10 @@ public class HotelListFragment extends ListFragment {
         super.onActivityCreated(savedInstanceState);
         mHoteis = carregarHoteis();
 
-        mAdapter = new ArrayAdapter<Hotel>(getActivity(),android.R.layout.simple_list_item_1,mHoteis);
-        setListAdapter(mAdapter);
+        limparBusca();
+
+    /*    mAdapter = new ArrayAdapter<Hotel>(getActivity(),android.R.layout.simple_list_item_1,mHoteis);
+        setListAdapter(mAdapter);*/
 
     }
     private List<Hotel> carregarHoteis(){
@@ -57,5 +60,28 @@ public class HotelListFragment extends ListFragment {
     }
     public interface AoClicarNoHotel{
         void clicouNoHotel(Hotel hotel);
+    }
+    public void buscar(String s){
+        if (s==null || s.trim().equals("")){
+            limparBusca();
+            return;
+        }
+        List<Hotel> hoteisEncontrados = new ArrayList<Hotel>(mHoteis);
+        for (int i=hoteisEncontrados.size()-1;i>-0;i--){
+            Hotel hotel = hoteisEncontrados.get(i);
+            if (!hotel.getNome().toUpperCase().contains(s.toUpperCase())){
+                hoteisEncontrados.remove(hotel);
+            }
+        }
+        mAdapter = new ArrayAdapter<Hotel>(getActivity(),android.R.layout.simple_list_item_1,hoteisEncontrados);
+        setListAdapter(mAdapter);
+    }
+    public void limparBusca(){
+        mAdapter = new ArrayAdapter<Hotel>(getActivity(),android.R.layout.simple_list_item_1,mHoteis);
+        setListAdapter(mAdapter);
+    }
+    public void adicionar(Hotel hotel){
+        mHoteis.add(hotel);
+        mAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/pratica/curso/com/hotelaria/MainActivity.java
+++ app/src/main/java/pratica/curso/com/hotelaria/MainActivity.java
@@ -1,16 +1,26 @@
 package pratica.curso.com.hotelaria;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements HotelListFragment.AoClicarNoHotel{
-
+public class MainActivity extends AppCompatActivity implements HotelListFragment.AoClicarNoHotel, SearchView.OnQueryTextListener,
+        MenuItemCompat.OnActionExpandListener{
+    private FragmentManager mFragmentManager;
+    private HotelListFragment mListFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFragmentManager = getSupportFragmentManager();
+        mListFragment = (HotelListFragment) mFragmentManager.findFragmentById(R.id.fragmentLista);
     }
 
     @Override
@@ -18,5 +28,43 @@ public class MainActivity extends AppCompatActivity implements HotelListFragment
         Intent it = new Intent(this,HotelDetalheActivity.class);
         it.putExtra(HotelDetalheActivity.EXTRA_HOTEL,hotel);
         startActivity(it);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_hotel,menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        searchView.setQueryHint(getString(R.string.hint_busca));
+        MenuItemCompat.setOnActionExpandListener(searchItem,this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        mListFragment.buscar(newText);
+        return false;
+    }
+
+    @Override
+    public boolean onMenuItemActionExpand(MenuItem item) {
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemActionCollapse(MenuItem item) {
+        mListFragment.limparBusca();
+        return true;
     }
 }
